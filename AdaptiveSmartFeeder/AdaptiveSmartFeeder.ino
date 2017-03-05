d5d52144139dc10c8f207d703bcc552c545cf42a
Edvaldo Junior <dujunior1300@gmail.com>
2017-03-05 12:50:04 -0300

    Add a delay when is under hole

--- AdaptiveSmartFeeder/AdaptiveSmartFeeder.ino
+++ AdaptiveSmartFeeder/AdaptiveSmartFeeder.ino
@@ -6,18 +6,13 @@
 #define HALFSTEP 8
 
 // Motor pin definitions
-<<<<<<< HEAD
 #define motorPin1  9      // IN1 on the ULN2003 driver 1
-=======
-#define motorPin1  9     // IN1 on the ULN2003 driver 1
->>>>>>> 9ac935d6d003b41e256dc14521d5eac0c5c51d8b
 #define motorPin2  10     // IN2 on the ULN2003 driver 1
 #define motorPin3  11     // IN3 on the ULN2003 driver 1
 #define motorPin4  12     // IN4 on the ULN2003 driver 1
 
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
 AccelStepper stepper1(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
-<<<<<<< HEAD
 
 Q2HX711 hx711(A3, A2);
 
@@ -26,11 +21,10 @@ long weightBeforeActivation = 0;
 
 bool isUnderHole = false;
 bool shouldMove = false;
-bool firstMove = true;
 
 int target = -1075;
 int desiredMass = 0;
-int massOfPlate = 77;
+int massOfPlate = 135;
 
 long getWeight() {
 
@@ -38,11 +32,7 @@ long getWeight() {
   diff = (diff / 3.08 - massOfPlate);
   return diff > 0 ? diff : 0;
 }
-=======
->>>>>>> 9ac935d6d003b41e256dc14521d5eac0c5c51d8b
 
-int target = -1175; //empírico
-int lackingMass = 0;
 // Esquemático:
 // https://evothings.com/control-an-led-using-hm-10-ble-module-an-arduino-and-a-mobile-app/
 
@@ -65,7 +55,6 @@ void setup() {
   stepper1.setMaxSpeed(1000.0);
   stepper1.setAcceleration(200.0);
   stepper1.setSpeed(300);
-<<<<<<< HEAD
 
   weightBeforeActivation = getWeight();
 }
@@ -85,6 +74,7 @@ void controlMotor() {
       }
       delay(200);
     } else {
+      delay(1000);
       target *= -1;
       stepper1.moveTo(stepper1.currentPosition() + target);
     }
@@ -93,15 +83,10 @@ void controlMotor() {
   stepper1.run();
 }
 
-=======
-}
-
->>>>>>> 9ac935d6d003b41e256dc14521d5eac0c5c51d8b
 void loop() {
 
   int c;
 
-<<<<<<< HEAD
   controlMotor();
   //stepper1.run();
 
@@ -109,19 +94,6 @@ void loop() {
       weightBeforeActivation = getWeight();
 
   if(/*my*/Serial.available()) {
-=======
-  if(lackingMass > 0 && stepper1.distanceToGo() == 0) {
-    target *= -1;
-    stepper1.moveTo(stepper1.currentPosition() + target);
-    
-    lackingMass -= 50; //Suponha que cairam 50 g
-    Serial.print("--->> = ");
-    Serial.println(lackingMass);
-  }
-  stepper1.run();
-
-  if(mySerial.available()) {
->>>>>>> 9ac935d6d003b41e256dc14521d5eac0c5c51d8b
     
     /*  Comandos vindos do iPhone vem em forma de string.
      *  É necessário definir o formato dos comandos para
@@ -157,7 +129,6 @@ void loop() {
         isUnderHole = false;
       }
     }
-<<<<<<< HEAD
     //Debugs
     else if (comando == "we"){
       Serial.print("\n\n----------\n\n");
@@ -180,17 +151,6 @@ void loop() {
       target *= -1;
       stepper1.moveTo(stepper1.currentPosition() + target);
     } else {
-=======
-    else if(comando.startsWith("ac ")) {
-      comando.remove(0, 3);
-      int desiredMass = comando.toInt();
-      Serial.print("  desiredMass = ");
-      Serial.println(desiredMass);
-
-      lackingMass = desiredMass;
-    }
-    else {
->>>>>>> 9ac935d6d003b41e256dc14521d5eac0c5c51d8b
       Serial.println("Mas nada aconteceu");
       mySerial.println("oi");
     }
